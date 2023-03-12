a3328afd13ae463f938115d2642234875b68e440
Josefa Miller <josefamillerb@gmail.com>
2023-03-12 15:55:40 -0300

    agregando funcionalidad

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,8 +6,30 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			
+
+		<div className="ticTacToe">
+			<h1>Tic Tac Toe <br /> ChiquillaMiller</h1>
+			<div className="buttons">
+				<button>Start</button>
+				<button>Reset</button>
+			</div>
+			<div className="board"><div className="row">
+				<div className="col-4">x</div>
+				<div className="col-4">x</div>
+				<div className="col-4">x</div>
+			</div>
+				<div className="row">
+					<div className="col-4">x</div>
+					<div className="col-4">x</div>
+					<div className="col-4">x</div>
+				</div>
+				<div className="row">
+					<div className="col-4">x</div>
+					<div className="col-4">x</div>
+					<div className="col-4">x</div>
+				</div>
+			</div>
+
 		</div>
 	);
 };
